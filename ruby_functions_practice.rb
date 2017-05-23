fc0227962c628c267fdef389c696f6c99489f88e
Jamie Johnstone <jcjohnstone@gmail.com>
2017-05-23 22:15:55 +0100

    First commit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,28 @@
+def return_10()
+  return "10".to_i
+end
+
+def add(first_number, second_number)
+  return first_number + second_number
+end
+puts add(1, 2)
+
+def subtract(first_number, second_number)
+  return first_number - second_number
+end
+puts subtract(10, 5)
+
+def multiply(first_number, second_number)
+  return first_number * second_number
+end
+puts multiply(4, 2)
+
+def divide(first_number, second_number)
+  return first_number / second_number
+end
+puts divide(10, 2)
+
+
+
+
+
